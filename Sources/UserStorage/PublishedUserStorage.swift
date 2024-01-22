bbecd5f92d1da1b4e3dcf9bc93337bc821d2f0e2
Mario Heubach <mac303@users.noreply.github.com>
2024-01-22 16:29:49 +0100

    make wrapper and stoe service public (#1)

--- Sources/UserStorage/PublishedUserStorage.swift
+++ Sources/UserStorage/PublishedUserStorage.swift
@@ -2,17 +2,17 @@ import Foundation
 import Combine
 
 @propertyWrapper
-struct PublishedUserStorage<T> where T: Codable {
+public struct PublishedUserStorage<T> where T: Codable {
     private var value: T
     private let key: String
     private var userStorageService = UserStorageService()
 
-    init( wrappedValue defaultValue: T, _ key: String) {
+    public init( wrappedValue defaultValue: T, _ key: String) {
         self.key = key
         self.value = userStorageService.load(key: key) ?? defaultValue
     }
 
-    var wrappedValue: T {
+    public var wrappedValue: T {
         get {
             value
         }
